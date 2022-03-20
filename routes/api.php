b8ad302b27cbb53b70d4b0f4ec7f3d122273b633
Saldi Sucipto <saldisucipto@gmail.com>
2022-03-20 11:33:53 +0700

    Fixing Route

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::prefix('com-info')->group(function () {
+    Route::get('/index', function () {
+        return response()->json("This Is A Route Wokring", 200);
+    });
 });
--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 // Parsing All Route to Vuejs Router
 Route::get('/{catchall?}', function () {
     return response()->view('welcome');
-})->where('catchall', '(.*)');
+})->where('catchall', '^(?!api).*$');
