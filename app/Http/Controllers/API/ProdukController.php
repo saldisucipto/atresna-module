26987f5e46ff7ec44f6fcd478ffbaa1b2c52155c
saldisucipto <saldisucipto@gmail.com>
2022-07-02 09:15:22 +0700

    Just Updated

--- app/Http/Controllers/API/ProdukController.php
+++ app/Http/Controllers/API/ProdukController.php
@@ -1,14 +1,15 @@
 <?php
-
 namespace App\Http\Controllers\API;
 
+use App\Http\Classes\FilesHandling;
 use App\Http\Controllers\Controller;
 use App\Models\KategoriProduk;
 use Illuminate\Http\Request;
+use Str;
 
 class ProdukController extends Controller
 {
-    // kategori produk
+// kategori produk
     public function kat_index(Request $req)
     {
         if ($req->isMethod('get')) {
@@ -19,7 +20,19 @@ class ProdukController extends Controller
             return response()->json(['data' => $data, 'message' => 'berhasil memperoleh data'], 200);
         } else {
             $dataParsing = $req->all();
+            $fileHandling = new FilesHandling();
+            $katProduk = new KategoriProduk();
+            $katProduk->slugs = Str::slug($dataParsing['nama_kat_produk']);
+            $katProduk->nama_kat_produk = $dataParsing['nama_kat_produk'];
+            $katProduk->deskripsi_kat_produk = $dataParsing['deskripsi_kat_produk'];
+            $katProduk->images_kat_produk = $fileHandling->upload($req->file('images_kat_produk'), 'kategori-produk', 'IMG-KAT-PRODUK');
+            $katProduk->save();
+            return response()->json(['data' => $dataParsing, 'message' => 'berhasil Membuat data'], 201);
         }
-
+    }
+// update dan get Sepesial Data
+    public function updateData(Request $req, $slugs = null)
+    {
+        $data = FilesHandling::find($slugs);
     }
 }
